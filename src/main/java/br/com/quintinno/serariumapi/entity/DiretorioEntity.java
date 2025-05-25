db4216a60be3414d5bf3bfd87c624f84cf316589
José Quintinno <josequintino@hotmail.com.br>
2025-05-25 20:07:02 -0300

    feature: Implementar mapeamento de dados

---
+++ src/main/java/br/com/quintinno/serariumapi/entity/DiretorioEntity.java
@@ -0,0 +1,108 @@
+package br.com.quintinno.serariumapi.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_diretorio")
+public class DiretorioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "code", nullable = false)
+    private Long code;
+
+    @Column(name = "code_diretorio_pai", nullable = false)
+    private Long codeDiretorioPai;
+
+    @Column(name = "nome", length = 255, nullable = false)
+    private String nome;
+
+    @Column(name = "rotulo", length = 100, nullable = false)
+    private String rotulo;
+
+    @Column(name = "tamanho", length = 10, nullable = false)
+    private String tamanho;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    public DiretorioEntity() {}
+
+    public Long getCode() {
+        return code;
+    }
+
+    public void setCode(Long code) {
+        this.code = code;
+    }
+
+    public Long getCodeDiretorioPai() {
+        return codeDiretorioPai;
+    }
+
+    public void setCodeDiretorioPai(Long codeDiretorioPai) {
+        this.codeDiretorioPai = codeDiretorioPai;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRotulo() {
+        return rotulo;
+    }
+
+    public void setRotulo(String rotulo) {
+        this.rotulo = rotulo;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public LocalDateTime getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(LocalDateTime deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+
+}
